495ad8da401876749d9a755c087c82e577bb36de
wenqing <wenqingli819@gmail.com>
2021-01-19 14:22:03 -0500

    done tdd

--- src/test/java/ReadCSVDataTest.java
+++ src/test/java/ReadCSVDataTest.java
@@ -1,13 +1,6 @@
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 /*
  * Created by babydeveloper on 1/15/21.
  */
@@ -18,17 +11,21 @@ public class ReadCSVDataTest {
     @Test
     public void whenCellHasQuotedStringOrEmbededComma() {
 
-        String testInput = ", 11854, Lunch, Oven-warmed Food";
-        String[] expectedOutput = new String[3];
+        String testInput = ",11854,\"        Lunch,         Oven-warmed Food, Reserve Amenity\",6036798789,6/22/12  ";
+        String[] expectedOutput = new String[5];
         expectedOutput[0] = "";
         expectedOutput[1] = "11854";
-        expectedOutput[2] = "Lunch, Oven-warmed Food";
+        expectedOutput[2] = "Lunch, Oven-warmed Food, Reserve Amenity";
+        expectedOutput[3] = "6036798789";
+        expectedOutput[4] = "6/22/12";
 
-        Parser parser = new Parser();
-        String[] firstRow = parser.parseLineUsingCode(testInput);
+        int lineAfterParserSize = Parser.parseLineUsingCode(testInput).length;
+        int expectedSize = 5;
+        assertThat(lineAfterParserSize).isEqualTo(expectedSize);
+
+        String[] firstRow = Parser.parseLineUsingCode(testInput);
         assertThat(firstRow).isEqualTo(expectedOutput);
+
     }
-
-
 
 }
